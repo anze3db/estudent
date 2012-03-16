3bb3fc1ee408911c6e31b6d6e2c27db9d109a26b
Smotko <ensmotko@gmail.com>
2012-03-16 08:34:25 +0100

    Set up static_media directory for static files

--- estudentDjango/settings.py
+++ estudentDjango/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'site_media') 
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,7 +72,8 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+	os.path.join(PROJECT_PATH, 'site_media')
+				# Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
@@ -148,3 +149,4 @@ LOGGING = {
         },
     }
 }
+STATIC_DOC_ROOT = PROJECT_PATH + '/site_media'
--- estudentDjango/urls.py
+++ estudentDjango/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+		        {'document_root': settings.STATIC_DOC_ROOT}),
     (r'', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'estudentDjango.views.home', name='home'),
