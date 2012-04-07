9cef9393363a31893f97ceb7944d3931a13afda0
Smotko <ensmotko@gmail.com>
2012-04-07 13:08:55 +0200

    Refactored the Countries model

--- estudentDjango/codelist/admin.py
+++ estudentDjango/codelist/admin.py
@@ -1,10 +1,7 @@
+from codelist.models import Country
 from django.conf.urls.defaults import *
 from django.contrib import admin, messages
 from django.shortcuts import redirect
-from urllib import urlopen
-from urllib import urlencode
-from codelist.models import Country
-from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext as _
 
 class CountryAdmin(admin.ModelAdmin):
@@ -18,25 +15,7 @@ class CountryAdmin(admin.ModelAdmin):
 
     def admin_update_countries(self, request):
         
-        csv_data = urlopen(self.UPDATE_URL, data=urlencode(
-				{'__EVENTTARGET':'lbtnCSV',
-				 '__VIEWSTATE':'/wEPDwUKMTE0NzM0NDIwOA8WAh4IRmlsZU5hbWUFDERSWkFWRV8yMDA3YRYCAgUPZBYCAgEPDxYCHgRUZXh0BUZEUlpBVkUgLSBEcsW+YXZlIGluIGRydWdhIG96ZW1samEgLSBzbG92ZW5za2kgc3RhbmRhcmQgSVNPIDMxNjYsIDIwMDdhZGRkGIb41rYrP0v4AjxvttSQlHzBYkKrRV1AnnRqhWi3Khc=',
-				 '__EVENTVALIDATION':'/wEWBgLjvsyeCgL9jOOoBAKy46z3AQK9zJaBCQLs0bLrBgKM54rGBqOqwJFQsH2QbRBir2QR/GT3KuQJWO2Z+1fyMQB26Y8j'
-						
-				}))
-        csv_data.readline()
-        
-        # remove all the data from the table:
-        Country.objects.all().delete();
-        
-        for line in csv_data.readlines():
-            l = line.split(';')
-            c = Country()
-            print l[1], str(l[2]), l[3]
-            c.category_code = l[1].strip()
-            c.descriptor = smart_unicode(l[2].strip(), encoding='windows-1250', strings_only=False, errors='strict')
-            c.descriptor_english = l[3].strip()
-            c.save()
+        Country.updateAll()
             
         messages.success(request, _("Countries added successfully"))
         
--- estudentDjango/codelist/models.py
+++ estudentDjango/codelist/models.py
@@ -1,19 +1,41 @@
 from django.db import models
+from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext as _
+from urllib import urlopen, urlencode
 
 
 # Create your models here.
 class Country(models.Model):
-	category_code = models.CharField(max_length=3)
-	descriptor    = models.CharField(max_length=255)
-	descriptor_english = models.CharField(max_length=255)
-	
-	class Meta:
-		verbose_name_plural = _("countries")
-		verbose_name = _("country")
-		
-	def __unicode__(self):
-		return self.descriptor
-	
-	
-		
+    category_code = models.CharField(max_length=3)
+    descriptor = models.CharField(max_length=255)
+    descriptor_english = models.CharField(max_length=255)
+    
+    class Meta:
+        verbose_name_plural = _("countries")
+        verbose_name = _("country")
+        
+    def __unicode__(self):
+        return self.descriptor
+    
+    @classmethod
+    def updateAll(cls):
+        UPDATE_URL = 'http://www.stat.si/klasje/tabela.aspx?CVN=3888'
+        
+        csv_data = urlopen(UPDATE_URL, data=urlencode(
+                {'__EVENTTARGET':'lbtnCSV',
+                 '__VIEWSTATE':'/wEPDwUKMTE0NzM0NDIwOA8WAh4IRmlsZU5hbWUFDERSWkFWRV8yMDA3YRYCAgUPZBYCAgEPDxYCHgRUZXh0BUZEUlpBVkUgLSBEcsW+YXZlIGluIGRydWdhIG96ZW1samEgLSBzbG92ZW5za2kgc3RhbmRhcmQgSVNPIDMxNjYsIDIwMDdhZGRkGIb41rYrP0v4AjxvttSQlHzBYkKrRV1AnnRqhWi3Khc=',
+                 '__EVENTVALIDATION':'/wEWBgLjvsyeCgL9jOOoBAKy46z3AQK9zJaBCQLs0bLrBgKM54rGBqOqwJFQsH2QbRBir2QR/GT3KuQJWO2Z+1fyMQB26Y8j'
+                        
+                }))
+        csv_data.readline()
+        
+        # remove all the data from the table:
+        Country.objects.all().delete();
+        
+        for line in csv_data.readlines():
+            l = line.split(';')
+            c = Country()
+            c.category_code = l[1].strip()
+            c.descriptor = smart_unicode(l[2].strip(), encoding='windows-1250', strings_only=False, errors='strict')
+            c.descriptor_english = l[3].strip()
+            c.save()
