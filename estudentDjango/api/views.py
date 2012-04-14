29907481789f3ee9e3784ee082803fca06d417c9
Smotko <ensmotko@gmail.com>
2012-04-14 20:19:33 +0200

    Cleaned up the login code a bit

--- estudentDjango/api/views.py
+++ estudentDjango/api/views.py
@@ -1,14 +1,13 @@
 # Create your views here.
-from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from failedloginblocker.models import FailedAttempt
 from student.models import Student
-from warnings import catch_warnings
 import json
 
 def login(request):
     user = request.GET['id']
-    response = { 'name': "", 'surname':"", 'login': False, 'numTries':0, 'errors':"" }
+    response = {'name': "", 'surname':"", 'login': False, 'numTries':0, 'errors':""}
+   
     try:
         fa = FailedAttempt.objects.get(username=user)
 
@@ -19,11 +18,10 @@ def login(request):
                 response['numTries'] = fa.failures
                 response['errors'] = "Too many login attempts."
                 return HttpResponse(json.dumps(response), mimetype="application/json")
-        
     except:
         fa = None
-        
-    auth = Student.authStudent(user, request.GET['password'])#None#authenticate(username=user, password=)
+    
+    auth = Student.authStudent(user, request.GET['password'])
     
     if auth:
         response["name"] = auth.name
@@ -33,7 +31,8 @@ def login(request):
         try:
             fa = FailedAttempt.objects.get(username=user)
         except:
-            pass
+            fa = None  
+            
         fa = fa or FailedAttempt( username=user, failures=0 )
         fa.failures += 1
         fa.save()
@@ -41,26 +40,3 @@ def login(request):
         response['errors'] = "Wrong username or password."
                 
     return HttpResponse(json.dumps(response), mimetype="application/json")
-
-
-def _checkLoginTries(user, password):
-
-    fa = FailedAttempt.objects.get( username=user )
-    if fa.recent_failure( ):
-        if fa.too_many_failures( ):
-            # block the authentication attempt because
-            # of too many recent failures
-            fa.failures += 1
-            fa.save( )
-            return
-            # raise LoginBlockedError( )
-    else:
-        # the block interval is over, reset the count
-        fa.failures = 0
-        fa.save( )
-    # authentication failed 
-    fa = fa or FailedAttempt( username=user, failures=0 )
-    fa.failures += 1
-    fa.save( )
-    # return with unsuccessful auth
-    return None
