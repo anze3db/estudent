3b6178435c02057daf5fdb9c1737747bf447171f
Smotko <ensmotko@gmail.com>
2012-05-23 17:29:25 +0200

    Fixed auth data (admin was not present), fixed startserver.sh

--- estudentDjango/startserver.sh
+++ estudentDjango/startserver.sh
@@ -4,7 +4,7 @@ if [ "$1" == "db" ]
 then
 	rm estudent.db
 	yes no | python manage.py syncdb
-	python loaddata auth/fixtures/initial_data.json
+	python manage.py loaddata auth/fixtures/initial_data.json
 fi
 
 python manage.py runserver
