ddeae0f148b97dd5ebe805634a4c6fffd03faf65
Smotko <ensmotko@gmail.com>
2012-05-28 19:38:56 +0200

    Added correct context to exam grade index

--- estudentDjango/student/views.py
+++ estudentDjango/student/views.py
@@ -12,18 +12,7 @@ def exam_grades_index(request):
     output = list(exam_dates)
     #latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     #output = ', '.join([p.question for p in latest_poll_list])
-    t = loader.get_template('admin/student/exam_grades_index.html')
-    c = Context({
-        'izpitni_roki': exam_dates,
-        })
-
-    return HttpResponse(t.render(c))
-
-
-    #return render_to_response('admin/student/exam_grades_index.html', {}, RequestContext(request))
-
-
-
+    return render_to_response('admin/student/exam_grades_index.html', {'izpitni_roki': exam_dates,}, RequestContext(request))
 
 #http://stackoverflow.com/questions/4148923/is-it-possible-to-create-a-custom-admin-view-without-a-model-behind-it
 def exam_grades_view(request, exam_Id): #show list of all objects
